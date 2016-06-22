0e9364b1d9a8ba418da106e86ceff497418e5f9d
ShadowStarKitty <ShadowStarKitty@users.noreply.github.com>
2016-06-22 11:59:26 +0100

    re-add safari

--- scripts.js
+++ scripts.js
@@ -21,7 +21,7 @@ var Config = {
     bfbot: "BF-bot",
     // safaribot: "Tauros",
     // suspectvoting.js available, but not in use
-    Plugins: ["mafia.js", "amoebagame.js", "tourstats.js", "trivia.js", "tours.js", "newtourstats.js", "auto_smute.js", "battlefactory.js", "hangman.js", "blackjack.js", "mafiastats.js", "mafiachecker.js"],
+    Plugins: ["mafia.js", "amoebagame.js", "tourstats.js", "trivia.js", "tours.js", "newtourstats.js", "auto_smute.js", "battlefactory.js", "hangman.js", "blackjack.js", "mafiastats.js", "mafiachecker.js", "safari.js"],
     Mafia: {
         bot: "Charizard",
         norepeat: 5,
